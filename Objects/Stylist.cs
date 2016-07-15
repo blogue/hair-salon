6cf18bb48ce378538b0f3013b05981388a392e04
Ben Logue <benjamin.logue73@gmail.com>
2016-07-15 09:54:39 -0700

    add first test to assert database empty on initialization and necessary methods.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace HairSalon
+namespace HairSalon.Objects
 {
   public class Stylist
   {
@@ -16,11 +16,55 @@ namespace HairSalon
       _id = Id;
     }
 
+    public int GetId()
+    {
+      return _id;
+    }
     public string GetName()
     {
       return _name;
     }
 
-    
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr = null;
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      rdr = cmd.ExecuteReader();
+      
+      while(rdr.Read())
+      {
+        int newStylistId = rdr.GetInt32(0);
+        string newStylistName = rdr.GetString(1);
+
+        Stylist newStylist = new Stylist(newStylistName, newStylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (rdr != null) rdr.Close();
+      if (conn != null) conn.Close();
+
+      return allStylists;
+    }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool stylistIdEquality = (_id == newStylist.GetId());
+        bool stylistNameEquality = (_name == newStylist.GetName());
+        return (stylistNameEquality && stylistIdEquality);
+      }
+    }
+
   }
 }
--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -1,21 +1,31 @@
 using Xunit;
+using HairSalon.Objects;
 using System.Collections.Generic;
 using System;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace HairSalon
+namespace HairSalon.Objects
 {
-  public class StylistTest : IDisposable
+  public class StylistTest
   {
-    public HairSalonTest()
+    public StylistTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
 
-    public void Dispose()
+    // public void Dispose()
+    // {
+    //   Stylist.DeleteAll();
+    // }
+
+    [Fact]
+    public void Stylist_DatabaseEmpty_0()
     {
-      Stylist.DeleteAll();
+      //Arrange, Act
+      int result = Stylist.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
     }
   }
 }
