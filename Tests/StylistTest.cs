d8e3fec48c11c924a745d1d96d26890b7f98e1be
Ben Logue <benjamin.logue73@gmail.com>
2016-07-15 10:19:12 -0700

    pass fourth test save to database with id.

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -41,13 +41,26 @@ namespace HairSalon.Objects
     [Fact]
     public void Stylist_SaveStylistToDatabase()
     {
-      //Arrange, Act
+      //Arrange
       Stylist testStylist = new Stylist("Johnny");
+      //Act
       testStylist.Save();
       Stylist savedStylist = Stylist.GetAll()[0];
       //Assert
       Assert.Equal(testStylist, savedStylist);
+    }
 
+    [Fact]
+    public void Stylist_SaveStylistWithId()
+    {
+      //Arrange
+      Stylist newStylist = new Stylist("Johnny");
+      //Act
+      newStylist.Save();
+      int result = Stylist.GetAll()[0].GetId();
+      int expectedResult = newStylist.GetId();
+      //Assert
+      Assert.Equal(result, expectedResult);
     }
   }
 }
